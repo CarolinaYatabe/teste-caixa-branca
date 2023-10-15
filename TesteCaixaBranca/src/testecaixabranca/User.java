0ee12d43fd661367a56b8faf3e8fc42e0d922a79
CarolinaYatabe <137456087+CarolinaYatabe@users.noreply.github.com>
2023-10-15 19:15:33 -0300

    Add files via upload

--- TesteCaixaBranca/src/testecaixabranca/User.java
+++ TesteCaixaBranca/src/testecaixabranca/User.java
@@ -7,44 +7,37 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class User {
-    //1
+    //Estabelecer uma conexão com o banco de dados
     public Connection conectarBD(){
-        //2
         Connection conn = null;
         try{
             Class.forName("com.mysql.Driver.Manager").newInstance();
+            //Define a url de conexão com o banco de dados
             String url = "jdbc:mysql://127.0.0.1/test?user=lopes&password=123";
+            //Estabelece conexão com o banco de dados
             conn = DriverManager.getConnection(url);
-        }catch (Exception e){ }
-        return conn;}
-    public String nome="";
-    public boolean result = false;
+        }catch (Exception e){ } //Tratamento de exceções
+        return conn;}//Retorna a conexão
+    public String nome="";//Variável que armazena o nome do usuário
+    public boolean result = false; //Variável que armazena o resultado da verificação
     public boolean verificarUsuario(String login, String senha){
-        //3
         String sql = "";
-        //4
-        Connection conn = conectarBD();
-        //INSTRUÇÃO SQL
-        //5
+        Connection conn = conectarBD(); //Estabelece conexão com o bvanco de dados
+        //Consulta DQL para verificar o usuário com base no login e senha
         sql += "select nome from usuários ";
         sql += "where login = " + "'" + login + "'";
-        sql += "  and senha  = " + "'" + senha  + "';";
-    //6    
+        sql += "  and senha  = " + "'" + senha  + "';";  
     try{
-    //7
-        //8
+        //Cria um Statement para executar a consulta no banco de dados
         Statement st = conn.createStatement();
+        //Executa a  consulta SQL e obtém resultados (ResultSet)
         ResultSet rs = st.executeQuery(sql);
         if(rs.next()){
-            //8
+            //Se  houver resultados, ddefine o resultado com true
             result = true;
-            //9
+            //Obtém o nome do usuário
             nome = rs.getString("nome");}
-            //10
-        //11 - result = false
-        //12 - fim do resultado
-        //13
-        }catch (Exception e){ }
-        //14
+        }catch (Exception e){ } //Trata exceções
+        //retorna o resultado ad verificação do usuário
         return result;}
     }
